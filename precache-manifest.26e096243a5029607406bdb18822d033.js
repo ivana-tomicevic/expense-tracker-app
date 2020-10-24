c491102fbfbf663e3d13f05004d583efc164955c
ivana-tomicevic <tomicevicivi@gmail.com>
2020-10-24 14:52:33 +0200

    Updates

---
+++ precache-manifest.26e096243a5029607406bdb18822d033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b548d1eebb8454d817c07b74f96b75",
+    "url": "/expense-tracker-app/index.html"
+  },
+  {
+    "revision": "f66d7ba71cb5b0d7c816",
+    "url": "/expense-tracker-app/static/css/main.3119c9bd.chunk.css"
+  },
+  {
+    "revision": "2a203eafb677978c8703",
+    "url": "/expense-tracker-app/static/js/2.56d10c26.chunk.js"
+  },
+  {
+    "revision": "9a8a64d30a81695e7691dd446a703cba",
+    "url": "/expense-tracker-app/static/js/2.56d10c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66d7ba71cb5b0d7c816",
+    "url": "/expense-tracker-app/static/js/main.395628b0.chunk.js"
+  },
+  {
+    "revision": "5f6fad31e2ce8e3fd61d",
+    "url": "/expense-tracker-app/static/js/runtime-main.ade9976e.js"
+  }
+]);
